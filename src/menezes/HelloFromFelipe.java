444f57d1f5447de89d531c9142e6672a4396b25e
Felipe Menezes <fmenezes@byui.edu>
2021-10-09 16:36:34 -0400

    Initial commit

---
+++ src/menezes/HelloFromFelipe.java
@@ -0,0 +1,13 @@
+package menezes;
+
+public class HelloFromFelipe {
+
+    public void sayHello() {
+        System.out.println("Hello From Felipe!");
+    }
+    public static void main(String[] args) {
+	// write your code here
+        new HelloFromFelipe().sayHello();
+
+    }
+}
