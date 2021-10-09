6cf61d88185b3c7f397f6b5ab2c9c8ae7dbc4fd4
Felipe Menezes <fmenezes@byui.edu>
2021-10-09 16:39:29 -0400

    New conversation phrase!

--- src/menezes/HelloFromFelipe.java
+++ src/menezes/HelloFromFelipe.java
@@ -4,6 +4,7 @@ public class HelloFromFelipe {
 
     public void sayHello() {
         System.out.println("Hello From Felipe!");
+        System.out.println("Goodbye!");
     }
     public static void main(String[] args) {
 	// write your code here
